ba8a11d13f11f22190482b2655631444fc16a21a
radha <radha@param-solutions.com>
2018-04-03 18:18:44 +0530

    First commit

---
+++ ValidLogger/ValidLogger/Helper.swift
@@ -0,0 +1,18 @@
+//
+//  Helper.swift
+//  ValidLogger
+//
+//  Created by Orbysol on 4/3/18.
+//  Copyright © 2018 Orbysol. All rights reserved.
+//
+
+import Foundation
+
+public class Helper {
+    
+    private init() {}
+    
+    public static func TestHelper() -> String {
+        return "Testing VALID LOGGER"
+    }
+}
---
+++ ValidLogger/ValidLogger/ValidLogger.h
@@ -0,0 +1,19 @@
+//
+//  ValidLogger.h
+//  ValidLogger
+//
+//  Created by Orbysol on 4/3/18.
+//  Copyright © 2018 Orbysol. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ValidLogger.
+FOUNDATION_EXPORT double ValidLoggerVersionNumber;
+
+//! Project version string for ValidLogger.
+FOUNDATION_EXPORT const unsigned char ValidLoggerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ValidLogger/PublicHeader.h>
+
+
